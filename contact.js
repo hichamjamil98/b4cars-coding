0b06e7c18fbcb80f447decb8bd4df8b2e3ed21f3
Better Mistakes <128420357+hichamjamil98@users.noreply.github.com>
2026-08-05 08:22:56 +0100

    updates

---
+++ contact.js
@@ -0,0 +1,14 @@
+/* ==========================================================================
+   B4CARS — CONTACT PAGE
+========================================================================== */
+
+(() => {
+    "use strict";
+  
+    document.addEventListener("DOMContentLoaded", () => {
+      const contactPage = document.querySelector(".section.is--contact");
+      if (!contactPage) return;
+  
+      document.body.classList.add("is--contact-page");
+    });
+  })();
